2dd753d38249feb08a4e71fdb41b0568f61d7fe1
Chris Eld <chris.eld95@gmail.com>
2020-04-09 23:20:00 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import Footer from '../src/components/Footer';
+import Header from '../src/components/Header';
+import Home from '../src/components/Home';
+import About from '../src/components/About';
+import Project from '../src/components/Project';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      {/* <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router> */}
+
+
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Project/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Project() {
+  return (
+    <div className="project">
+      <span>Pupster 2017</span>
+    </div>
+  );
+}
+
+export default Project;
